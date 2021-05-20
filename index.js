3c60692faf527e883ae51dffc748c66b6e2ef093
aishwarya-bm <biradaraishwarya@gmail.com>
2021-05-20 23:40:54 +0530

    refactor: code formatting and text changes
    code formatting and few text changes

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ function playGame()
     
   }
 }
-var username = readlinesync.question(chalk.bold.black.bgGreen("Welcome to the quiz - Do you know me?? \nPlease tell us your name\n"))
+var username = readlinesync.question(chalk.bold.black.bgGreen("Welcome to the quiz - Do you know Aishwarya?? \nPlease tell us your name\n"))
 console.log(chalk.bold.red("Hi ",username, ", lets check the rules before we begin the game."))
-console.log(chalk.bold.red("Lets play a fun game where you have to answer few questions about XYZ.\n1.The correct answer will give you 5 points.\n2.The wrong answer will give you 0 points.\n3.You can type your answer and press enter. Please dont give spaces.\nLets begin the fun!!!"));
+console.log(chalk.bold.red("Lets play a fun game where you have to answer few questions about Aishwarya to see how well do you know your friend.\n1.The correct answer will give you 5 points.\n2.The wrong answer will give you 0 points.\n3.You can type your answer and press enter. Please dont give spaces.\nLets begin the fun!!!"));
 console.log(chalk.yellow("**************************"));
 playGame();
